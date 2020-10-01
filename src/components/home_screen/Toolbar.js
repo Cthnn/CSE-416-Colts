ef963d7f131dbc9355a64ae2d784902709eb2e92
NotDanielG <d.goon9608@gmail.com>
2020-10-01 15:42:57 -0400

    Fixed CSS errors, need to update for better UX

--- src/components/home_screen/Toolbar.js
+++ src/components/home_screen/Toolbar.js
@@ -7,27 +7,31 @@ class Toolbar{
         this.container.className = 'toolbar';
 
         this.left_container = document.createElement('div');
-        this.left_text = document.createElement('span');
+        this.left_text = document.createElement('p');
         this.left = document.createElement('input');
 
+        this.left_container.className = 'sub-container';
         this.left_text.textContent = 'Districts';
+        this.left_text.className = 'text';
         this.left.type = 'checkbox';
         this.left.className = 'view-button';
 
         this.right_container = document.createElement('div');
-        this.right_text = document.createElement('span');
+        this.right_text = document.createElement('p');
         this.right = document.createElement('input');
+
+        this.right_text.className = 'text';
+        this.right_container.className = 'text';
         this.right_text.textContent = 'Precinct';
         this.right.type = 'checkbox';
         this.right.className = 'view-button';
         
         this.left_container.appendChild(this.left_text);
         this.left_container.appendChild(this.left);
-        this.right_container.appendChild(this.right_text);
-        this.right_container.appendChild(this.right);
+        this.left_container.appendChild(this.right_text);
+        this.left_container.appendChild(this.right);
 
         this.container.append(this.left_container);
-        this.container.append(this.right_container);
 
         return this.container;
     }
