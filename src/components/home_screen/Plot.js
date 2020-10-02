ae6909aac099623d3320553cb83c9fc20a496215
Cthnn <ethan@toysdiva.com>
2020-10-02 01:45:06 -0400

    Converted old plot to box plot

--- src/components/home_screen/Plot.js
+++ src/components/home_screen/Plot.js
@@ -1,22 +1,56 @@
 import React from 'react';
 import Chart from "chart.js";
+import 'chartjs-chart-box-and-violin-plot/build/Chart.BoxPlot.js';
 class Plot extends React.Component {
     constructor(props) {
         super(props);
         this.chartRef = React.createRef();
     }
     componentDidMount() {
-        this.myChart = new Chart(this.chartRef.current, {
-          type: 'bar',
-          data: {
-            labels: ['A', 'B', 'C', 'D'],
-            datasets: [{
-                label: 'My data',
-                data: [10, 20, 30, 40],
-                backgroundColor: '#112233'
+      const boxplotData = {
+        labels: ['District 1', 'District 2', 'District 3', 'District 4', 'District 5', 'District 6', 'District 7','District 8','District 9','District 10'],
+        datasets: [{
+          label: 'Disctricts',
+          backgroundColor: 'rgba(255,0,0,0.5)',
+          borderColor: 'red',
+          borderWidth: 1,
+          outlierColor: '#999999',
+          padding: 10,
+          itemRadius: 0,
+          data: [
+            Array.from({length:100}).map(() => Math.random()*(25)+0),
+            Array.from({length:100}).map(() => Math.random()*(25)+15),
+            Array.from({length:100}).map(() => Math.random()*(25)+30),
+            Array.from({length:100}).map(() => Math.random()*(25)+45),
+            Array.from({length:100}).map(() => Math.random()*(25)+60),
+            Array.from({length:100}).map(() => Math.random()*(25)+75),
+            Array.from({length:100}).map(() => Math.random()*(25)+90),
+            Array.from({length:100}).map(() => Math.random()*(25)+105),
+            Array.from({length:100}).map(() => Math.random()*(25)+120),
+            Array.from({length:100}).map(() => Math.random()*(25)+135),
+          ]
+        }]
+      };
+      this.myChart = new Chart(this.chartRef.current, {
+        type: 'boxplot',
+        data: boxplotData,
+        options:{
+          scales:{
+            yAxes:[{
+              scaleLabel:{
+                display:true,
+                labelString: 'Voting Age Population %'
+              }
+            }],
+            xAxes:[{
+              scaleLabel:{
+                display:true,
+                labelString: 'Districts'
+              }
             }]
           }
-        });
+        }
+      });
       }
     render() {
       return <canvas ref={this.chartRef}/>
